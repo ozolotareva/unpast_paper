4d23a833426f7f760cab375b92ef36b0569f6ce7
AndiMajore <andi.majore@googlemail.com>
2022-08-30 17:29:03 +0200

    fixing output of summary scripts for biclustering evaluation

--- evaluation/biclustering/collect_results.py
+++ evaluation/biclustering/collect_results.py
@@ -6,19 +6,21 @@ def collect(out_dir):
     for tool in os.listdir(out_dir):
         tool_path = os.path.join(out_dir, tool)
         for config in os.listdir(tool_path):
+            if '.txt' in config:
+                continue
             config_path = os.path.join(tool_path, config)
             with open(os.path.join(tool_path, f'summary_{tool}_{config}.txt'), 'w') as fw:
-                runs = defaultdict(lambda _: dict())
+                runs = defaultdict(dict)
                 for result in os.listdir(config_path):
                     if 'summary' not in result:
                         run = result.split("-")[1].split(".")[0]
                         infile = result.split("-")[0]
                         with open(os.path.join(config_path, result), 'r') as fr:
                             for line in fr.readlines():
-                                runs[infile][run] = line
+                                runs[infile][run] = float(line)
                 for infile in runs.keys():
-                    scores = list(runs[infile])
+                    scores = list(runs[infile].values())
                     scores.sort()
-                    score_string = "\t".join(scores)
-                    fw.write(f'{infile}\t{score_string}\n')
+                    score_string = ", ".join(map(str, scores))
+                    fw.write(f'{infile}\t{(sum(scores)/len(scores))}\t{score_string}\n')
     print("Created summaries")
