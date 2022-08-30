5c7b2a010240badf657618df3a3c4ef4bc4d0726
fernando <fernando.miguel.delgado-chaves@uni-hamburg.de>
2022-08-30 14:11:20 +0200

    clustering algorithms evaluation

---
+++ evaluation/clustering/run_WGCNAkmeans.py
@@ -0,0 +1,16 @@
+import sys, os
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+#os.chdir('/Users/fernando/Documents/Research/DESMOND2/methods')
+
+args = sys.argv
+input_file = args[1]
+results_dir = args[2]
+
+# results_dir = '/Users/fernando/Documents/Research/DESMOND2/methods/results/results_WGCNAkmeans/'
+# input_file = '/Users/fernando/Documents/Research/DESMOND2/datasets/DESMOND2_data_simulated/simulated/A/example.tsv'
+dimreduced_input = input_file.replace('.tsv', '_MEs.tsv')
+
+if not os.path.exists(dimreduced_input):
+    print('Running R WGCNA script')
+    os.system(f"Rscript --vanilla run_WGCNA.R {input_file}")
+os.system(f"python3 run_kmeans.py {dimreduced_input} {results_dir}")
---
+++ evaluation/clustering/run_kmeans.py
@@ -0,0 +1,53 @@
+import sys, os
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+from kneed import KneeLocator
+import matplotlib.pyplot as plt
+os.environ["OMP_NUM_THREADS"] = "1"
+#  python3 run_kmeans.py ../datasets/DESMOND2_data_simulated/simulated/A/example.tsv  results
+
+
+def reformat_cluster_results(clusters, input_df):
+    res_reformat = []
+    for labelno in range(max(clusters.label) + 1):
+        # print(labelno)
+        samples = set(clusters[clusters.label == labelno]['sample'].astype(str))
+        n_samples = len(samples)
+        frac = n_samples / input_df.shape[0]
+        res_reformat.append([samples, n_samples, frac])
+    res_reformat = pd.DataFrame(res_reformat, columns=['samples', 'n_samples', 'frac_samples'])
+    return res_reformat
+
+
+args = sys.argv
+input_file = args[1]
+results_dir = args[2]
+# results_dir = '/Users/fernando/Documents/Research/DESMOND2/methods/results/results_kmeans/'
+# input_file = '/Users/fernando/Documents/Research/DESMOND2/datasets/DESMOND2_data_simulated/A.n_genes=500,m=4,std=1,overlap=no.exprs_z.tsv'
+# input_file = '/Users/fernando/Documents/Research/DESMOND2/datasets/DESMOND2_data_simulated/simulated/A/example_MEs.tsv'
+df = pd.read_csv(input_file, sep='\t', index_col=0).T
+
+cs = []
+for k in tqdm(range(1, 21)):
+    est_kmeans = KMeans(n_clusters=k, init='k-means++', max_iter=300, n_init=20, random_state=0)
+    est_kmeans.fit(df)
+    result_k = reformat_cluster_results(clusters=pd.DataFrame({'sample': df.index, 'label': est_kmeans.labels_}), input_df=df)
+    result_k.to_csv(results_dir + input_file.split('/')[-1].replace('.tsv', f'_k_{k}.tsv'), sep='\t')
+    cs.append(est_kmeans.inertia_)
+
+'''
+#optional plotting
+plt.plot(range(1, 21), cs, '-o')
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('CS')
+plt.show()
+'''
+kn = KneeLocator(range(len(cs)), cs, curve='convex', direction='decreasing')
+if kn.knee:
+    print('Found a knee')
+    est_kmeans = KMeans(n_clusters=kn.knee, init='k-means++', max_iter=300, n_init=20, random_state=0)
+    est_kmeans.fit(df)
+    result_knee = reformat_cluster_results(clusters=pd.DataFrame({'sample': df.index, 'label': est_kmeans.labels_}), input_df=df)
+    result_knee.to_csv(results_dir + input_file.split('/')[-1].replace('.tsv', f'_knee_{kn.knee}.tsv'), sep='\t')
