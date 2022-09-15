a59ef5a83720b2277b3d621d689fd0afd9d3baee
fmdelgado <ferdecha@gmail.com>
2022-09-15 10:12:27 +0200

    Changed file transfer

--- evaluation/clustering/run_eval.sh
+++ evaluation/clustering/run_eval.sh
@@ -3,4 +3,4 @@
 # docker-compose build; docker-compose up -d; ./run_eval.sh
 docker cp /local/DESMOND2_data_simulated/simulated desmond2_eval_container_clustering:/local/DESMOND2_data_simulated/
 screen docker exec -it desmond2_eval_container_clustering python run_clusters.py 1
-#docker cp desmond2_eval_container_clustering:/tmp/ ~/ENCORE_functions/clustering/
+docker cp desmond2_eval_container_clustering:/tmp/ ~/ENCORE_functions/clustering/
