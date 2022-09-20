0d65c796e730cfa55005bdcbba1732d0e5276568
fmdelgado <ferdecha@gmail.com>
2022-09-20 20:07:36 +0200

    Changed results final folder

--- evaluation/clustering/run_eval.sh
+++ evaluation/clustering/run_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ssh bbb1417@llaima.zbh.uni-hamburg.de
 # docker-compose build; docker-compose up -d; screen ./run_eval.sh
-docker cp /local/DESMOND2_data_simulated/simulated desmond2_eval_container_clustering:/local/DESMOND2_data_simulated/
-screen docker exec -it desmond2_eval_container_clustering python run_clusters.py 1
+docker cp /local/DESMOND2_data_simulated/simulated desmond2_eval_container_clustering2:/local/DESMOND2_data_simulated/
+screen docker exec -it desmond2_eval_container_clustering2 python run_clusters.py 1
 # screen docker exec -it desmond2_eval_container_clustering python run_clusters_realdata.py 1
-docker cp desmond2_eval_container_clustering:/tmp/ ~/ENCORE_functions/clustering/
+docker cp desmond2_eval_container_clustering2:/tmp/ ~/ENCORE_functions/clustering/
