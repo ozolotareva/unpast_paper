72ede3cebbd0a3c475e73fb0dfeea09b835e45a2
Hartung <bba1401@node041.zbh.uni-hamburg.de>
2023-10-27 15:39:35 +0200

    update asthma

--- evaluation/factorization/best_parameters.py
+++ evaluation/factorization/best_parameters.py
@@ -16,55 +16,35 @@ class BestBrcaTCGA: # PAM50
     iClusterPlus = {'lambda_n': 5, 'n_cluster': 11, 'lambda_scale': 1, 'iter_max': 20, 'eps': 0.0001, 'type': 'gaussian', 'burnin_n': 200, 'draw_n': 200, 'sdev': 0.05}
     
     
+class OptimizedBRCAForAsthma: # average ranked, on BRCA (TCGA & METABRIC)
+    NMF = {'k': 2, 'init': 'nndsvda', 'tol': 0.0001, 'transposed': False, 'alpha_W': -0.1, 'alpha_H': 0.0, 'shuffle': False, 'solver': 'cd', 'beta_loss': 'frobenius', 'max_iter': 1000}
+    moCluster = {'n_dimensions': 5, 'n_cluster': 2, 'solver': 'svd', 'center': True, 'method': 'globalScore', 'option': 'uniform', 'scale': False, 'k': 1}
+    iClusterPlus = {'lambda_n': 10, 'n_cluster': 2, 'lambda_scale': 1, 'iter_max': 20, 'eps': 0.0001, 'type': 'gaussian', 'burnin_n': 200, 'draw_n': 200, 'sdev': 0.05}
+    MOFA2 = {'n_factors': 12, 'n_cluster': 2, 'ard_weights': True, 'ard_factors': False, 'likelihood': 'gaussian', 'spikeslab_weights': True, 'spikeslab_factors': False}
+    sparse_PCA = {'n_components': 2, 'alpha': 1, 'ridge_alpha': 0.001, 'max_iter': 1000, 'method': 'cd', 'tol': 1e-08}
+ 
+class OptimizedBRCAForAsthmaARI: 
+    # average ranked, on BRCA (TCGA & METABRIC)
+    NMF = {'k': 2, 'init': 'nndsvda', 'tol': 0.0001, 'transposed': False, 'alpha_W': -0.1, 'alpha_H': 0.0, 'shuffle': False, 'solver': 'cd', 'beta_loss': 'frobenius', 'max_iter': 200}
+    moCluster = {'n_dimensions': 5, 'n_cluster': 2, 'solver': 'svd', 'center': True, 'method': 'globalScore', 'option': 'lambda1', 'scale': False, 'k': 1}
+    iClusterPlus = {'lambda_n': 25, 'n_cluster': 2, 'lambda_scale': 1, 'iter_max': 20, 'eps': 0.0001, 'type': 'gaussian', 'burnin_n': 200, 'draw_n': 200, 'sdev': 0.05}
+    MOFA2 = {'n_factors': 2, 'n_cluster': 2, 'ard_weights': True, 'ard_factors': False, 'likelihood': 'gaussian', 'spikeslab_weights': True, 'spikeslab_factors': False}
+    sparse_PCA = {'n_components': 2, 'alpha': 1, 'ridge_alpha': 0.1, 'max_iter': 1000, 'method': 'cd', 'tol': 1e-08}
+ 
 class OptimizedBRCA: # average ranked, on BRCA (TCGA & METABRIC)
-    NMF = {'k': 2, #8
-    'init': 'nndsvda',
-    'tol': 0.0001,
-    'alpha_W': -0.1,
-    'alpha_H': 0.0,
-    'shuffle': False,
-    'solver': 'cd',
-    'beta_loss': 'frobenius',
-    'max_iter': 1000}
-
-    iClusterPlus = {
-    'n_cluster': 2, #5
-    'type': 'gaussian',
-    'burnin_n': 200,
-    'draw_n': 200,
-    'lambda_n': 'null',
-    'iter_max': 20,
-    'sdev': 0.05,
-    'eps': 0.0001,
-    'lambda_scale': 1}
-
-    moCluster = {'n_dimensions': 2,
-    'n_cluster': 2, # 8
-    'random_state': 1,
-    'solver': 'fast',
-    'center': True,
-    'method': 'globalScore',
-    'option': 'inertia',
-    'scale': False,
-    'k': 0.1}
-
-    MOFA2 = {'n_factors': 18,
-    'n_cluster': 2, #5
-    'random_state': 3,
-    'ard_weights': True,
-    'ard_factors': False,
-    'likelihood': 'gaussian',
-    'spikeslab_weights': True,
-    'spikeslab_factors': False}
-
-    sparse_PCA = {'n_components': 2, # 20
-    'random_state': 1,
-    'alpha': 5,
-    'ridge_alpha': 0.001,
-    'max_iter': 1000,
-    'method': 'cd',
-    'tol': 1e-08}
-    
+    NMF = {'k': 5, 'init': 'nndsvda', 'tol': 0.0001, 'transposed': False, 'alpha_W': -0.1, 'alpha_H': 0.0, 'shuffle': False, 'solver': 'cd', 'beta_loss': 'frobenius', 'max_iter': 1000}
+    moCluster = {'n_dimensions': 5, 'n_cluster': 5, 'solver': 'svd', 'center': True, 'method': 'globalScore', 'option': 'uniform', 'scale': False, 'k': 1}
+    iClusterPlus = {'lambda_n': 10, 'n_cluster': 5, 'lambda_scale': 1, 'iter_max': 20, 'eps': 0.0001, 'type': 'gaussian', 'burnin_n': 200, 'draw_n': 200, 'sdev': 0.05}
+    MOFA2 = {'n_factors': 12, 'n_cluster': 5, 'ard_weights': True, 'ard_factors': False, 'likelihood': 'gaussian', 'spikeslab_weights': True, 'spikeslab_factors': False}
+    sparse_PCA = {'n_components': 5, 'alpha': 1, 'ridge_alpha': 0.001, 'max_iter': 1000, 'method': 'cd', 'tol': 1e-08}
+   
+   
+class OptimizedBRCA: # average ranked, on BRCA (TCGA & METABRIC)
+    NMF = {'k': 8, 'init': 'nndsvda', 'tol': 0.0001, 'transposed': False, 'alpha_W': -0.1, 'alpha_H': 0.0, 'shuffle': False, 'solver': 'cd', 'beta_loss': 'frobenius', 'max_iter': 1000}
+    moCluster = {'n_dimensions': 5, 'n_cluster': 13, 'solver': 'svd', 'center': True, 'method': 'globalScore', 'option': 'uniform', 'scale': False, 'k': 1}
+    iClusterPlus = {'lambda_n': 10, 'n_cluster': 12, 'lambda_scale': 1, 'iter_max': 20, 'eps': 0.0001, 'type': 'gaussian', 'burnin_n': 200, 'draw_n': 200, 'sdev': 0.05}
+    MOFA2 = {'n_factors': 12, 'n_cluster': 9, 'ard_weights': True, 'ard_factors': False, 'likelihood': 'gaussian', 'spikeslab_weights': True, 'spikeslab_factors': False}
+    sparse_PCA = {'n_components': 9, 'alpha': 1, 'ridge_alpha': 0.001, 'max_iter': 1000, 'method': 'cd', 'tol': 1e-08}
     
 class DefaultAsthma:
     NMF = {'k': 2,
